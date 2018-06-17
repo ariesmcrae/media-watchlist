37bc960f6135b003a25e1d1e35c2a5056dcf4aec
Aries McRae <amcrae@transurban.tech>
2018-06-17 18:01:07 +1000

    Add input binding

--- src/app/media-item/media-item.component.ts
+++ src/app/media-item/media-item.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-media-item',
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent { }
+export class MediaItemComponent {
+  @Input() mediaItem;
+
+  onDelete() {
+    console.log('xxx deleted');
+  }
+}
